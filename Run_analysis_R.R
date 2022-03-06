e9481eeba57221d14ba18fec481cc94f6c54e009
Barbonto <cbarbon93@gmail.com>
2022-03-06 16:38:21 -0500

    code updated

--- Run_analysis_R.R
+++ Run_analysis_R.R
@@ -15,14 +15,14 @@ if (!file.exists("UCI HAR Dataset")) {
 }
  
 # Create a dataframe for each data set
-features <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/features.txt", col.names = c("n","functions"))
-activities <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
-subject_test <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
-x_test <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
-y_test <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/test/y_test.txt", col.names = "code")
-subject_train <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
-x_train <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
-y_train <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/train/y_train.txt", col.names = "code")
+features <- read.table("UCI HAR Dataset/features.txt", col.names = c("n","functions"))
+activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
+x_test <- read.table("UCI HAR Dataset/test/X_test.txt", col.names = features$functions)
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt", col.names = "code")
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt", col.names = "subject")
+x_train <- read.table("UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
 # merge x/y test and train data by ROWs
 X <- rbind(x_train, x_test)
