12bf8dfa22256d6d83d0cecee357b90b5a0da02d
Barbonto <cbarbon93@gmail.com>
2022-03-06 16:21:36 -0500

    change variable names and adding comments

--- Run_analysis_R.R
+++ Run_analysis_R.R
@@ -1,8 +1,9 @@
 library(dplyr)
 
+# assign file name to viriable filename
 filename <- "Coursera_DS3_Final.zip"
 
-# Checking if archieve already exists.
+# Checking if document already exists.
 if (!file.exists(filename)){
   fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
   download.file(fileURL, filenFame, method="curl")
@@ -12,7 +13,8 @@ if (!file.exists(filename)){
 if (!file.exists("UCI HAR Dataset")) { 
   unzip(filename) 
 }
-
+ 
+# Create a dataframe for each data set
 features <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/features.txt", col.names = c("n","functions"))
 activities <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/activity_labels.txt", col.names = c("code", "activity"))
 subject_test <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/test/subject_test.txt", col.names = "subject")
@@ -22,30 +24,33 @@ subject_train <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins
 x_train <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/train/X_train.txt", col.names = features$functions)
 y_train <- read.table("C:/Users/crist/Documents/Data Science/Johns Hopkins/Getting and Cleaning Data/Project/Cleaning-Data-Project/UCI HAR Dataset/train/y_train.txt", col.names = "code")
 
+# merge x/y test and train data by ROWs
 X <- rbind(x_train, x_test)
 Y <- rbind(y_train, y_test)
 Subject <- rbind(subject_train, subject_test)
+# merge Subject, X and Y dataframes
 Merged_Data <- cbind(Subject, Y, X)
+#Extracts only the measurements on the mean and standard deviation for each measurement
+cleandata <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
+#replaces codes to activity names from the activities dataframe
+cleandata$code <- activities[cleandata$code, 2]
 
-TidyData <- Merged_Data %>% select(subject, code, contains("mean"), contains("std"))
-
-TidyData$code <- activities[TidyData$code, 2]
-
-names(TidyData)[2] = "activity"
-names(TidyData)<-gsub("Acc", "Accelerometer", names(TidyData))
-names(TidyData)<-gsub("Gyro", "Gyroscope", names(TidyData))
-names(TidyData)<-gsub("BodyBody", "Body", names(TidyData))
-names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
-names(TidyData)<-gsub("^t", "Time", names(TidyData))
-names(TidyData)<-gsub("^f", "Frequency", names(TidyData))
-names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
-names(TidyData)<-gsub("-mean()", "Mean", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-std()", "STD", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-freq()", "Frequency", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("angle", "Angle", names(TidyData))
-names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
-
-FinalData <- TidyData %>%
+#Uses descriptive activity names to name the activities in the data set
+names(cleandata)[2] = "activity"
+names(cleandata)<-gsub("Acc", "Accelerometer", names(cleandata))
+names(cleandata)<-gsub("Gyro", "Gyroscope", names(cleandata))
+names(cleandata)<-gsub("BodyBody", "Body", names(cleandata))
+names(cleandata)<-gsub("Mag", "Magnitude", names(cleandata))
+names(cleandata)<-gsub("^t", "Time", names(cleandata))
+names(cleandata)<-gsub("^f", "Frequency", names(cleandata))
+names(cleandata)<-gsub("tBody", "TimeBody", names(cleandata))
+names(cleandata)<-gsub("-mean()", "Mean", names(cleandata), ignore.case = TRUE)
+names(cleandata)<-gsub("-std()", "STD", names(cleandata), ignore.case = TRUE)
+names(cleandata)<-gsub("-freq()", "Frequency", names(cleandata), ignore.case = TRUE)
+names(cleandata)<-gsub("angle", "Angle", names(cleandata))
+names(cleandata)<-gsub("gravity", "Gravity", names(cleandata))
+#creates an unique dataframe set with the average of each variable for each activity and each subject.
+FinalData <- cleandata %>%
   group_by(subject, activity) %>%
   summarise_all(funs(mean))
 write.table(FinalData, "FinalData.txt", row.name=FALSE)
